f4a8efeb6382118c8426d790485542d0cff5a75d
deepak.dhull <deepak.dhull@verse.in>
2024-05-08 22:07:50 +0530

    init hydra config

---
+++ main.py
@@ -0,0 +1,15 @@
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+from common.hydra.util import init_hydra
+
+
+
+@hydra.main(version_base=None, config_path="config")
+def main_fn(cfg: DictConfig) -> None:
+    obj = OmegaConf.to_object(cfg)
+    print(obj)
+
+if __name__ == '__main__':
+    init_hydra()
+    main_fn()
