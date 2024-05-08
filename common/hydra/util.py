42efc5e1ea2d62ba938dec4e64d20281975ea260
deepak.dhull <deepak.dhull@verse.in>
2024-05-08 22:12:53 +0530

    init hydra config

---
+++ common/hydra/util.py
@@ -0,0 +1,17 @@
+import hydra
+from hydra.core.plugins import Plugins
+from omegaconf import DictConfig, OmegaConf
+from hydra.core.config_search_path import ConfigSearchPath
+from hydra.plugins.search_path_plugin import SearchPathPlugin
+
+class HydraConfigsSearchPathPlugin(SearchPathPlugin):
+    def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
+        search_path.append(
+            provider="test", path="pkg://config/test"
+        )
+
+def init_hydra():
+    OmegaConf.register_new_resolver(
+        "eval", lambda x: eval(x)
+    )
+    Plugins.instance().register(HydraConfigsSearchPathPlugin)
